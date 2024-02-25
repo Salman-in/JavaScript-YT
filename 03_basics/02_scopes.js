331e9536426b3c50ebb7f899c77d9c87e1c63410
Salman <salmancodess@gmail.com>
2024-02-25 18:06:36 +0530

    Scopes Done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,58 +1,56 @@
-//var c = 300
+var c = 300 //Globally scoped 
+
 let a = 300
 if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
-    
+    let a = 10 //Block scoped
+    const b = 20 //Block scoped
+    var c = 30 //Globally scoped 
+    // console.log("INNER: ", a); 
 }
-
-
-
-// console.log(a);
+// console.log(a); //Prints only 300 , as a = 10 is block scoped.
 // console.log(b);
-// console.log(c);
+// console.log(c); //As var keyword is globally scoped , the variable declared inside the "if block" will be executed outside the block.
 
-
+//Scope in FUNCTIONS
 function one(){
     const username = "salman"
 
     function two(){
         const website = "youtube"
-        console.log(username);
+        console.log(username); //Will be able to print the 'username'//
     }
-    // console.log(website);
+    // console.log(website); //Will throw an error as 'website' is block scoped in fn 'two'//
 
-     two()
-
+      two() //Calling the fn 'two' //
 }
+// one() //Calling the fn 'one' //
 
-// one()
-
+//Scope in IF-ELSE
 if (true) {
     const username = "salman"
     if (username === "salman") {
         const website = " youtube"
         // console.log(username + website);
     }
-    // console.log(website);
+    // console.log(website); //Will throw an error//
 }
 
-// console.log(username);
+// console.log(username); //Will throw an error//
 
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
+// ++++++++++++++++++ Interesting cases(Function Hoisting basics) ++++++++++++++++++ //
 
-
-console.log(addone(5))
+// console.log(addone(5)) //In this case the declaration can be made before the function initialization//
 
 function addone(num){
     return num + 1
 }
 
 
+// console.log(addTwo(5));  //In this case the declaration cannot be made before the function initialization,as the function defined here is a bit different i.e, the function is declared to a variable 'addTwo' //
 
-addTwo(5)
 const addTwo = function(num){
     return num + 2
 }
+
+
