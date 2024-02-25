455abda18f4dfeba43e322322a4f7420fcd540a6
Salman <salmancodess@gmail.com>
2024-02-25 18:41:35 +0530

    Arrow functions done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,19 +1,19 @@
 const user = {
-    username: "salman",
+    username: "Salman",
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(`${this.username} , welcome to the website!`);
+        // console.log(this); //Prints the given context//
     }
-
 }
+// user.welcomeMessage() //First context //
+// user.username = "Sam" //Context is changed //
+// user.welcomeMessage() //Second context //
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+// console.log(this); //Prints an empty object as there is no given context//
 
-// console.log(this);
+
 
 // function chai(){
 //     let username = "salman"
@@ -27,28 +27,21 @@ const user = {
 //     console.log(this.username);
 // }
 
-const chai =  () => {
+// chai()
+
+//***************ARROW FUNCTION**************//
+const chai = () => {
     let username = "salman"
     console.log(this);
 }
-
-
 // chai()
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
-// }
+// } //Explicit return 
 
-// const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "salman"})
-
+// const addTwo = (num1, num2) =>  num1 + num2 //Implicit return (No need to write return keyword)
+// const addTwo = (num1, num2) => ( num1 + num2 ) //Implicit return (No need to write return keyword)
+const addTwo = (num1, num2) => ({username: "salman"}) //Returning an object
 
 console.log(addTwo(3, 4))
-
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
