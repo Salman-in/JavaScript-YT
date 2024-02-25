4848f463950f4f8bbeb6f27cd4519fddf50efbc0
Salman <salmancodess@gmail.com>
2024-02-25 19:12:36 +0530

    IIFE Done

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -7,6 +7,13 @@
 })();
 
 ( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('salman')
+    //Unnamed IIFE
+    console.log(`DB CONNECTED TO ${name}`);
+} )('SALMAN');
 
+//*********NOTES*********//
+/*Syntax:
+** ()() ----> 1st parenthesis is for function definition , 2nd parenthesis is for execution call for defined fn. Also,don't forget to use ; at the end **
+**This method is to execute the code immediately.
+**Also to eradicate global scope pollution in the code.
+*/
