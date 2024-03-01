e93adfb2ab219ca0fec5091b453b2315ca44fabb
Salman <salmancodess@gmail.com>
2024-03-01 17:13:05 +0530

    Switch Done

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,3 +1,4 @@
+// Basic Switch syntax //
 // switch (key) {
 //     case value:
         
@@ -14,13 +15,13 @@ switch (month) {
         console.log("January");
         break;
     case "feb":
-        console.log("feb");
+        console.log("Febraury");
         break;
     case "march":
-        console.log("march");
-        break;
+        console.log("March");
+        // break; //Here if we dont add break , it will automatically print the below cases even after matching with this case except default (Not Applicable for modern/latest languages).
     case "april":
-        console.log("april");
+        console.log("April");
         break;
 
     default:
