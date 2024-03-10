f819748e005d009124150e8d3a158354158b0daf
Salman <salmancodess@gmail.com>
2024-03-10 12:49:26 +0530

    Iterations One Done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,4 +1,4 @@
-// for
+// FOR LOOPS //
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
@@ -8,14 +8,14 @@ for (let i = 0; i <= 10; i++) {
     //console.log(element);
     
 }
-
-// console.log(element);
-
+// *********Loop inside loop*********
 for (let i = 1; i <= 10; i++) {
-    //console.log(`Outer loop value: ${i}`);
+    // console.log(`Outer loop value: ${i}`);
+    // console.log(`Multiplication table of ${i}`);
    for (let j = 1; j <= 10; j++) {
-    //console.log(`Inner loop value ${j} and inner loop ${i}`);
-    //console.log(i + '*' + j + ' = ' + i*j );
+    // console.log(`Inner loop value ${j} and outer loop ${i}`);
+    // console.log(i + '*' + j + ' = ' + i*j );
+    // console.log(`${i} * ${j} = ${i*j} `); //Clean code
    }
     
 }
@@ -23,27 +23,24 @@ let myArray = ["flash", "batman", "superman"]
 //console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    //console.log(element);
-    
+    // console.log(element);
 }
 
 
-// break and continue
+// ********break and continue*********
 
 // for (let index = 1; index <= 20; index++) {
 //     if (index == 5) {
 //         console.log(`Detected 5`);
-//         break
-//     }
+//         break //Breaks the loop at a given index//
+//     } //NOTE: If we don't use break it will also print the given index//
 //    console.log(`Value of i is ${index}`);
-    
 // }
 
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
         console.log(`Detected 5`);
-        continue
+        continue //Continues the loop after skipping the given index//
     }
-   console.log(`Value of i is ${index}`);
-    
+   console.log(`Value of i is ${index}`);  
 }
