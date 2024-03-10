75a5357ba6ab7e58d83aa79f623147313fcc69fb
Salman <salmancodess@gmail.com>
2024-03-10 12:24:43 +0530

    Truthy falsy commit

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -2,10 +2,11 @@ const userEmail = []
 
 if (userEmail) {
     console.log("Got user email");
-} else {
+} 
+else {
     console.log("Don't have user email");
 }
-
+////////////////////////33:00/////////////////////////
 // falsy values
 
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
@@ -17,11 +18,11 @@ if (userEmail) {
 //     console.log("Array is empty");
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
-}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
 
 // Nullish Coalescing Operator (??): null undefined
 
